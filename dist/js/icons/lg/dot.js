0278228e83e9e1ad4357751c6eecc492940cb9f6
mungodewar <89925955+mungodewar@users.noreply.github.com>
2024-04-16 10:34:41 +0100

    [BDES-144]: modify ai icon, add new dot icon (#72)

---
+++ dist/js/icons/lg/dot.js
@@ -0,0 +1,8 @@
+import React from "react";
+export default (({
+  styles = {},
+  ...props
+}) => <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" style={{
+  width: "1.5rem",
+  height: "1.5rem"
+}} {...props}><circle cx="12" cy="12" r="8" /></svg>);
